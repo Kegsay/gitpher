5832e4fec51914677a1b516ee4c67b8cc898e9c7
Daniel Wagner-Hall <dawagner@gmail.com>
2016-03-22 14:41:57 +0000

    Initial tweaks

--- src/github.com/kegsay/gitpher/github_api.go
+++ src/github.com/kegsay/gitpher/github_api.go
@@ -5,21 +5,20 @@ import (
 	"net/http"
 )
 
-// GithubUserContentURL The base URL to Github User Content
-const GithubUserContentURL = "https://raw.githubusercontent.com/"
+const githubUserContentURL = "https://raw.githubusercontent.com/"
 
-// GetGithubUserContent Fetch a raw file from Github.
-func GetGithubUserContent(contentPath string) (string, error) {
-	githubURL := GithubUserContentURL + contentPath
+// GetGithubUserContent fetches a raw file from Github.
+func GetGithubUserContent(contentPath string) ([]byte, error) {
+	githubURL := githubUserContentURL + contentPath
 	response, err := http.Get(githubURL)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	defer response.Body.Close()
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return string(body), nil
+	return body, nil
 }
